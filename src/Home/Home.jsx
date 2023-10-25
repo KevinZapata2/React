a499a00c9142329cf29c00e405674ca03dd98497
kevinzapata2 <kevinzapata2011@gmail.com>
2023-10-25 12:11:52 -0500

    ok

---
+++ src/Home/Home.jsx
@@ -0,0 +1,16 @@
+import './Home.css'
+import {Menu} from '../Menu/Menu'
+export function Home(){
+    return(
+    <>
+    <header>
+        <Menu></Menu>
+    </header>
+    <section className='banner'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ea molestiae tempore eveniet nam est enim cum quidem blanditiis. Sed tenetur, nostrum earum ea alias ipsa ex aperiam. Vitae, illo!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis quo fuga quas vero natus asperiores, provident modi id quia molestiae exercitationem incidunt quam inventore vitae numquam cum beatae corrupti similique!
+        </p>
+    </section>
+    </>
+    )
+}
